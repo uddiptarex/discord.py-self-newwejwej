cbb05935cb569baa556150ad8ab0bb3ddb6d9989
dolfies <jeyalfie47@gmail.com>
2025-02-24 05:07:21 -0500

    Create ScheduledEvent on cache miss in SCHEDULED_EVENT_DELETE

--- discord/state.py
+++ discord/state.py
@@ -3196,12 +3196,8 @@ class ConnectionState:
     def parse_guild_scheduled_event_delete(self, data: gw.GuildScheduledEventDeleteEvent) -> None:
         guild = self._get_guild(int(data['guild_id']))
         if guild is not None:
-            try:
-                scheduled_event = guild._scheduled_events.pop(int(data['id']))
-            except KeyError:
-                pass
-            else:
-                self.dispatch('scheduled_event_delete', scheduled_event)
+            scheduled_event = guild._scheduled_events.pop(int(data['id']), ScheduledEvent(state=self, data=data))
+            self.dispatch('scheduled_event_delete', scheduled_event)
         else:
             _log.debug('SCHEDULED_EVENT_DELETE referencing unknown guild ID: %s. Discarding.', data['guild_id'])
 
